cd75bccf17e4167f4cd2b44a1689308fc655d773
BRANDON PARKER <Brandon@dkparker.com>
2015-11-09 15:53:16 -0800

    Added JS functions to get Random Numbers/Photos. Console.log to make sure it works.

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
-var guitarPhotos []
+var guitarPhoto = []
 
 function GuitarPhoto (name, path) {
   this.name = name;
   this.path = path;
   this.votes = 0
-  images.push(this);
+  guitarPhoto.push(this);
 }
 
-
-}
 var tracker = {
   getRandomNum: function (){
-
-}
+    return Math.floor(Math.random() * 12) + 1;
+  },
   getRandomImg: function (){
-    for (var i = 0; i < guitarPhotos.length; i++) {
-      guitarPhotos[i]
-    };
+    var img1 = guitarPhoto[this.getRandomNum()].path;
+    var img2 = guitarPhoto[this.getRandomNum()].path;
 
-}
+  },
+};
 
 
-var img1 = new Photo(name, path)
-var img2 = new Photo(name, path)
-var img3 = new Photo(name, path)
-var img4 = new Photo(name, path)
-var img5 = new Photo(name, path)
-var img6 = new Photo(name, path)
-var img7 = new Photo(name, path)
-var img8 = new Photo(name, path)
-var img9 = new Photo(name, path)
-var img10 = new Photo(name, path)
-var img11 = new Photo(name, path)
-var img12 = new Photo(name, path)
-guitarPhotos();
+var img1 = new GuitarPhoto('Gibson 335', "images/335.jpg");
+var img2 = new GuitarPhoto('Angus Young SG', "images/ay-sg.jpg");
+var img3 = new GuitarPhoto('Beatles Rickenbacher', "images/b-rb.jpg");
+var img4 = new GuitarPhoto('BB King Lucille', "images/bbk-l.jpg");
+var img5 = new GuitarPhoto('PRS BlueGreen', "images/bluegreen-prs.jpg");
+var img6 = new GuitarPhoto('Brian May', "images/bm.jpg");
+var img7 = new GuitarPhoto('DC 400', "images/dc400.jpg");
+var img8 = new GuitarPhoto('DC 600', "images/dc600.jpg");
+var img9 = new GuitarPhoto('Kurt Cobain Jag', "images/kc-f.jpg");
+var img10 = new GuitarPhoto('Les Paul', "images/lp.jpg");
+var img11 = new GuitarPhoto('Slash Les Paul', "images/s-lp.jpg");
+var img12 = new GuitarPhoto('Van Halen Strat', "images/vh-s.jpg");
