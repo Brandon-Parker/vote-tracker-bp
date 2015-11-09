c8f133ec19527875927c4e32cedb9e48ab60ef71
BRANDON PARKER <Brandon@dkparker.com>
2015-11-09 15:08:30 -0800

    Initial git commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var guitarPhotos []
+
+function GuitarPhoto (name, path) {
+  this.name = name;
+  this.path = path;
+  this.votes = 0
+  images.push(this);
+}
+
+
+}
+var tracker = {
+  getRandomNum: function (){
+
+}
+  getRandomImg: function (){
+    for (var i = 0; i < guitarPhotos.length; i++) {
+      guitarPhotos[i]
+    };
+
+}
+
+
+var img1 = new Photo(name, path)
+var img2 = new Photo(name, path)
+var img3 = new Photo(name, path)
+var img4 = new Photo(name, path)
+var img5 = new Photo(name, path)
+var img6 = new Photo(name, path)
+var img7 = new Photo(name, path)
+var img8 = new Photo(name, path)
+var img9 = new Photo(name, path)
+var img10 = new Photo(name, path)
+var img11 = new Photo(name, path)
+var img12 = new Photo(name, path)
+guitarPhotos();
