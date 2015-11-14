d67a4c3839d743d8552c2265ac8c6f2595a681ad
BRANDON PARKER <Brandon@dkparker.com>
2015-11-14 13:00:16 -0800

    Final tweeks on css and styling. Added hyperlinks to websites for guitar photos.

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ if (localStorage.guitarPhoto) {
 } else {
   var guitarPhoto = [];
   var img1 = new GuitarPhoto('Kiesel Aqua Aries', "images/aqua-aries.jpg", "#2E9AFE");
-  var img2 = new GuitarPhoto('Blood Red PRS', "images/blackred-prs.jpg", "#B40404");
-  var img3 = new GuitarPhoto('Blue Green PRS', "images/bluegreen-prs.jpg", "#01DF3A");
+  var img2 = new GuitarPhoto('PRS Custom 24 Charcoal Tri-Color Burst', "images/blackred-prs.jpg", "#B40404");
+  var img3 = new GuitarPhoto('PRS Custom 24 Blue Green', "images/bluegreen-prs.jpg", "#01DF3A");
   var img4 = new GuitarPhoto('Kiesel DC7X', "images/dc7x.jpg", "#FE9A2E");
   var img5 = new GuitarPhoto('Carvin DC 400', "images/dc400.jpg", "#0040FF");
   var img6 = new GuitarPhoto('Kiesel Fire Aries', "images/fire-aries.jpg", "#FFFF00");
   var img7 = new GuitarPhoto('Kiesel Lava SCB7', "images/lava-scb7.jpg", "#FA58AC");
   var img8 = new GuitarPhoto('Gibson Les Paul Pro Black', "images/lespaul-b.jpg", "#585858");
   var img9 = new GuitarPhoto('Gibson Les Paul Vintage Burst', "images/lespaul.jpg", "#F5D0A9");
-  var img10 = new GuitarPhoto('Custom Single Cut PRS', "images/sc-prs.jpg", "#D358F7");
+  var img10 = new GuitarPhoto('PRS Private Stock SC245 Zombie Heart', "images/sc-prs.jpg", "#D358F7");
   var img11 = new GuitarPhoto('Suhr Modern', "images/suhr.jpg", "#B45F04");
   var img12 = new GuitarPhoto('Nashville Telecaster', "images/tele.jpg", "#BDBDBD");
 }
@@ -41,12 +41,16 @@ var tracker = {
    this.photo2 = guitarPhoto[this.getRandomNum()];
    var img1 = this.photo1.path;
    var img2 = this.photo2.path;
-   while (img1 === img2) {
-     this.photo2 = this.getRandomNum();
-     img2 = guitarPhoto[this.photo2].path;
+     while (img1 === img2) {
+       this.photo2 = this.getRandomNum();
+       img2 = guitarPhoto[this.photo2].path;
    };
    display1.src = img1;
    display2.src = img2;
+   var capLeft = document.getElementById('capLeft');
+   capLeft.innerHTML = this.photo1.name;
+   var capRight = document.getElementById('capRight');
+   capRight.innerHTML = this.photo2.name;
  },
 
 refreshSegments: function() {
